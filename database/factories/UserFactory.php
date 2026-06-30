22190037ee8900ce0b12558fe63a2c2f1fc5e3f2
Aravindan-001 <aravindans583224205003@nprcolleges.org>
2026-06-30 20:10:05 +0530

    fix laravel factory faker initialization

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => bcrypt('password'),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ];
     }
 
